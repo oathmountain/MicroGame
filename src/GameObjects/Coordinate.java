222d1bf8adbf6a0a48d3e43fabb98a511881d59b
victor m <victor.moback@gmail.com>
2018-06-20 16:40:22 +0200

    Add Coordinate class to GameObjects

---
+++ src/GameObjects/Coordinate.java
@@ -0,0 +1,22 @@
+package GameObjects;
+
+public class Coordinate {
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void move(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public Coordinate(int x, int y) {
+        this.x += x;
+        this.y += y;
+    }
+}
