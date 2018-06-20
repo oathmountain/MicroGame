b906d2cb6c9c627f9ff3b485c6e3b37ecdba8e5e
Jimmy Edberg <oathmountain@hotmail.com>
2018-06-20 16:40:58 +0200

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import GameObjects.Player;
 
 public class Main {
     public static void main(String[] args) {
-        InputListener input = new InputListener();
         GameEngine engine = new GameEngine();
         GameCharacter[] characters = new GameCharacter[1];
         characters[0] = new Player();
+
+        while(true){
+
+        }
     }
 }
