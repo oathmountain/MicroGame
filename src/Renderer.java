39046dab09cd01ff71c24b53bac2ef8ac932cbf2
Ludwig Hedeby <ludwig@hedeby.me>
2018-06-20 16:26:35 +0200

    Added render method

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,2 +1,12 @@
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
 public class Renderer {
+    public Renderer() {
+        Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+    }
+
 }
