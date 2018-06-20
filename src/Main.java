c55dbdc909cd39b435fd27e4f5f1abc8c1d6e391
Jimmy Edberg <oathmountain@hotmail.com>
2018-06-20 16:23:20 +0200

    Cleaned up Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 import GameObjects.GameCharacter;
 import GameObjects.Player;
-import com.googlecode.lanterna.*;
-import com.googlecode.lanterna.terminal.Terminal;
-
-import java.nio.charset.Charset;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +7,5 @@ public class Main {
         GameEngine engine = new GameEngine();
         GameCharacter[] characters = new GameCharacter[1];
         characters[0] = new Player();
-
-        Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
-        terminal.enterPrivateMode();
-
     }
 }
