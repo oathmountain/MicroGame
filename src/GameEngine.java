5e1010a0ac6462974600ecbd064a37adf2bfd757
Jimmy Edberg <oathmountain@hotmail.com>
2018-06-21 09:30:45 +0200

    Fixed missed return in keycheck

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -6,17 +6,19 @@ import com.googlecode.lanterna.input.Key;
 public class GameEngine {
     public void tick(Key key, GameCharacter[] characters) {
 
-        if(checkKey(key)){
-            (Player)characters[0].moveTo(key);
+        if (checkKey(key)) {
+            Player player = (Player) characters[0];
+            player.moveTo(key);
         }
     }
 
     private boolean checkKey(Key key) {
-        if(key.getKind() == Key.Kind.ArrowDown ||
+        if (key.getKind() == Key.Kind.ArrowDown ||
                 key.getKind() == Key.Kind.ArrowLeft ||
                 key.getKind() == Key.Kind.ArrowUp ||
-                key.getKind() == Key.Kind.ArrowRight){
-
+                key.getKind() == Key.Kind.ArrowRight) {
+            return true;
         }
+        return false;
     }
 }
